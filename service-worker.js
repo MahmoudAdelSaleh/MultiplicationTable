a0e6f2ba90717e78f923a90430a2510b894b9d5b
MahmoudAdelSaleh <mahmoudadelsaleh@outlook.com>
2025-08-04 18:47:47 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,23 @@
-const CACHE_NAME = 'multiplication-pwa-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('times-table-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './clap.mp3',
+        './wrong.mp3',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
